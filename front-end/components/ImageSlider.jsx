945980c644dd6f3e95f82a9d3edc342cb9748fc2
Ozy <munkhtamir.g@outlook.com>
2022-09-27 23:56:06 +0800

    Init

--- front-end/components/ImageSlider.jsx
+++ front-end/components/ImageSlider.jsx
@@ -3,7 +3,7 @@ import Carousel from "react-material-ui-carousel";
 import Item from "react-material-ui-carousel";
 
 export default function ImageSlider() {
-  const items = ["/banner1.sv", "/banner2.svg", "/banner3.svg"];
+  const items = ["/banner1.svg", "/banner2.svg", "/banner3.svg"];
   return (
     <Carousel style={{ width: "100vw" }}>
       {items.map((item, i) => (
